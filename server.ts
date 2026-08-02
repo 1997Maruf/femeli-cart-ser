4cb99cfcea1bc871f8315beba019d54a0171f16d
Maruf Hossen <mdmarufmeze@gmail.com>
2026-08-02 12:53:13 +0600

    fix vercel config

--- server.ts
+++ server.ts
@@ -454,12 +454,15 @@ app.delete(
   },
 );
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.VERCEL !== "1") {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
-
 export default app;
